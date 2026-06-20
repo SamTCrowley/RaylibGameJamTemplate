c756009301ebbd52b05c24bf94e6eb6af697d211
Sam <samtcrowley@gmail.com>
2026-06-20 04:59:20 -0500

    fixed a git add issue, maybe.

---
+++ src/raylib_game.cpp
@@ -0,0 +1,141 @@
+/*******************************************************************************************
+*
+*   raylib gamejam template
+*
+*   Template originally created with raylib 4.5-dev, last time updated with raylib 5.0
+*
+*   Template licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2022-2025 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include "raymath.h"
+
+#if defined(PLATFORM_WEB)
+    #define CUSTOM_MODAL_DIALOGS            // Force custom modal dialogs usage
+    #include <emscripten/emscripten.h>      // Emscripten library - LLVM to JavaScript compiler
+#endif
+
+#include <iostream>
+#include <stdio.h>                          // Required for: printf()
+#include <stdlib.h>                         // Required for: 
+#include <string.h>                         // Required for: 
+
+//----------------------------------------------------------------------------------
+// Defines and Macros
+//----------------------------------------------------------------------------------
+// Simple log system to avoid printf() calls if required
+// NOTE: Avoiding those calls, also avoids const strings memory usage
+#define SUPPORT_LOG_INFO
+#if defined(SUPPORT_LOG_INFO)
+    #define LOG(...) printf(__VA_ARGS__)
+#else
+    #define LOG(...)
+#endif
+
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+typedef enum { 
+    SCREEN_LOGO = 0, 
+    SCREEN_TITLE, 
+    SCREEN_GAMEPLAY, 
+    SCREEN_ENDING
+} GameScreen;
+
+// TODO: Define your custom data types here
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition
+//----------------------------------------------------------------------------------
+static const int screenWidth = 720;
+static const int screenHeight = 720;
+
+static RenderTexture2D target = { 0 };  // Render texture to render our game
+
+// TODO: Define global variables here, recommended to make them static
+
+
+//----------------------------------------------------------------------------------
+// Module Functions Declaration
+//----------------------------------------------------------------------------------
+static void UpdateDrawFrame(void);      // Update and Draw one frame
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+#if !defined(_DEBUG)
+    SetTraceLogLevel(LOG_ALL);         // Disable raylib trace log messages
+#endif
+
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "raylib gamejam template");
+    
+    // TODO: Load resources / Initialize variables at this point
+    
+    // Render texture to draw full screen, enables screen scaling
+    // NOTE: If screen is scaled, mouse input should be scaled proportionally
+    //target = LoadRenderTexture(screenWidth, screenHeight);
+    //SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+
+    Texture tex = LoadTexture("./src/resources/DerpDude.png");
+    if(IsTextureValid(tex) == false){
+        std::cout << "BAD TEXTURE!" << std::endl;
+    }
+    Vector2 tex_position{100.0f, 100.0f};
+    
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);     // Set our game frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button
+    {
+        if(IsKeyPressed(KEY_A)){
+            tex_position.x -= 10.0f;
+        }
+        if(IsKeyPressed(KEY_D)){
+            tex_position.x += 10.0f;
+        }
+        if(IsKeyPressed(KEY_W)){
+            tex_position.y -= 10.0f;
+        }
+        if(IsKeyPressed(KEY_S)){
+            tex_position.y += 10.0f;
+        }
+        //UpdateDrawFrame();
+        BeginDrawing();
+        ClearBackground(WHITE);
+        DrawText("HELLO", 100, 50, 24, BLACK);
+        DrawRectangle(100, 100, 64, 64, RED);
+        DrawTexture(tex, tex_position.x, tex_position.y, WHITE);
+        EndDrawing();
+        
+    }
+#endif
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadRenderTexture(target);
+    
+    // TODO: Unload all loaded resources at this point
+
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+//--------------------------------------------------------------------------------------------
+// Module Functions Definition
+//--------------------------------------------------------------------------------------------
+
