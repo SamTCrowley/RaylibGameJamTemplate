45f99248e0798458d9bab603fa44c846bb3524f0
Sam <samtcrowley@gmail.com>
2026-07-07 22:41:39 -0500

    Fixed alphadiscardWeb.fs again.

--- src/resources/alphadiscardWeb.fs
+++ src/resources/alphadiscardWeb.fs
@@ -10,7 +10,7 @@ uniform sampler2D texture0;
 
 void main()
 {
-    vec4 texelColor = texture(texture0, fragTexCoord);
+    vec4 texelColor = texture2D(texture0, fragTexCoord);
     if (texelColor.a == 0.0) discard;
     gl_FragColor = texelColor;
 }
