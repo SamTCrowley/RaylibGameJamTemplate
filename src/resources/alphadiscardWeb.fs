82a01db99a87496d42bcd8c975855bc84f1381e2
Sam <samtcrowley@gmail.com>
2026-07-07 22:21:25 -0500

    Fixed issue with alphadiscardWeb.fs shader, maybe.

--- src/resources/alphadiscardWeb.fs
+++ src/resources/alphadiscardWeb.fs
@@ -1,19 +1,16 @@
-#version 330
+#version 100
+precision mediump float;
 
-// Input vertex attributes (from vertex shader)
-in vec2 fragTexCoord;
-in vec4 fragColor;
+
+varying vec2 fragTexCoord;
 
 // Input uniform values
 uniform sampler2D texture0;
-uniform vec4 colDiffuse;
 
-// Output fragment color
-out vec4 finalColor;
 
 void main()
 {
     vec4 texelColor = texture(texture0, fragTexCoord);
     if (texelColor.a == 0.0) discard;
-    finalColor = texelColor * fragColor * colDiffuse;
+    gl_FragColor = texelColor;
 }
